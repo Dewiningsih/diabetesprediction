17eb258ac9cceb6be0731dd85210e2c701907f9d
Dewi Ayu Murtiningsih <146178390+Dewiningsih@users.noreply.github.com>
2025-06-11 14:32:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ st.title("Prediksi Diabetes")
  # Form input
 with st.form("form_diabetes"):
    pregnancies = st.number_input('Pregnancies', min_value=0, max_value=20, step=1)
-    glucose = st.number_input('Glucose', min_value=0, max_value=200)
-    blood_pressure = st.number_input('Blood Pressure', min_value=0, max_value=150)
-    skin_thickness = st.number_input('Skin Thickness', min_value=0, max_value=100)
-    insulin = st.number_input('Insulin', min_value=0, max_value=1000)
-    bmi = st.number_input('BMI', min_value=0.0, max_value=70.0)
-    dpf = st.number_input('Diabetes Pedigree Function', min_value=0.0, max_value=2.5)
-    age = st.number_input('Age', min_value=1, max_value=120)
-    submit = st.form_submit_button("Proses")
+   glucose = st.number_input('Glucose', min_value=0, max_value=200)
+   blood_pressure = st.number_input('Blood Pressure', min_value=0, max_value=150)
+   skin_thickness = st.number_input('Skin Thickness', min_value=0, max_value=100)
+   insulin = st.number_input('Insulin', min_value=0, max_value=1000)
+   bmi = st.number_input('BMI', min_value=0.0, max_value=70.0)
+   dpf = st.number_input('Diabetes Pedigree Function', min_value=0.0, max_value=2.5)
+   age = st.number_input('Age', min_value=1, max_value=120)
+   submit = st.form_submit_button("Proses")
  # Ketika tombol ditekan
- if submit:
+if submit:
     # Format input ke bentuk array
-    features = np.array([[pregnancies, glucose, 
+   features = np.array([[pregnancies, glucose, 
 blood_pressure, skin_thickness, insulin, bmi, dpf, age]])
     
     # Prediksi
